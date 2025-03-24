cace69c343434f2e6a78fa0c59d8f940fe2f8ea9
Souka001 <soukainaiziki2001@gmail.com>
2025-03-24 01:59:43 +0000

    modification on Footer

--- src/components/HomePage/footer.js
+++ src/components/HomePage/footer.js
@@ -4,9 +4,9 @@ import "./Styleshome.css";
 
 const Footer = () => {
   return (
-    <footer className="footer  ">
-      <Container className="container w-100" >
-        <Row  className="container w-100 footer">
+    <footer className="footer">
+      <Container>
+        <Row>
           <Col md={12}>
             <div className="fulltitlecnt">
               <h3 className="title">LUXORA</h3>
@@ -32,11 +32,9 @@ const Footer = () => {
           </Col>
         </Row>
         <Row>
-          <Col md={12} className="mb-4 mb-md-0" size={12}>
+          <Col md={12} className="mb-4 mb-md-0">
             <div className="pfooter">
-             
-              
-              <p >Privacy Policy</p>
+              <p>Privacy Policy</p>
               <p>Terms of Use</p>
               <p>Copyright © 2025 LUXORA. All rights reserved</p>
             </div>
@@ -55,7 +53,7 @@ const Footer = () => {
                   height="20"
                   rx="4"
                   stroke="#FEFEFE"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 />
                 <circle cx="18" cy="6" r="1" fill="#FEFEFE" />
                 <circle
@@ -63,7 +61,7 @@ const Footer = () => {
                   cy="12"
                   r="5"
                   stroke="#FEFEFE"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 />
               </svg>
               <svg
@@ -76,9 +74,9 @@ const Footer = () => {
                 <path
                   d="M13 1H10C7.23858 1 5 3.23858 5 6V8H1V12H5V19H9V12H13V8H9V6C9 5.44772 9.44771 5 10 5H13V1Z"
                   stroke="#FEFEFE"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
               <svg
@@ -95,13 +93,13 @@ const Footer = () => {
                   height="18"
                   rx="4"
                   stroke="#FEFEFE"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 />
                 <path
                   d="M10.4472 8.72361L15.2111 11.1056C15.9482 11.4741 15.9482 12.5259 15.2111 12.8944L10.4472 15.2764C9.78231 15.6088 9 15.1253 9 14.382V9.61803C9 8.87465 9.78231 8.39116 10.4472 8.72361Z"
                   stroke="#FEFEFE"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
                 />
               </svg>
             </div>
