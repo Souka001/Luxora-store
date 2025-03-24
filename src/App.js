2489b7f45dd6040de6fec55c516f1ada3b160190
Souka001 <soukainaiziki2001@gmail.com>
2025-03-24 01:11:18 +0000

    modification from routes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/" element={
+        <Route path="/#" element={
           <>
             <NavBar />
             <Banner />
